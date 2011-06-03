c196f27ed4254b52021bb192d1a8d555ff32469e
Felix Rinker <Felix.Rinker@tuwien.ac.at>
2011-06-04 01:24:15 +0200

    Implemented MapGrid and provider and detachablemodel without db support

--- swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/Map.java
+++ swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/Map.java
@@ -6,6 +6,7 @@ import org.apache.wicket.markup.repeater.data.GridView;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import at.ac.tuwien.swag.model.domain.Square;
 import at.ac.tuwien.swag.webapp.in.provider.MapDataProvider;
 
 public class Map extends InPage {
@@ -14,18 +15,20 @@ public class Map extends InPage {
     public Map(PageParameters parameters) {
         super(parameters);
         
-        IDataProvider<?> dataProvider = new MapDataProvider();
-        GridView<?> gridView = new GridView("rows", dataProvider) {
+        IDataProvider<Square> dataProvider = new MapDataProvider();
+        GridView<Square> gridView = new GridView<Square>("rows", dataProvider) {
         	
-        	@Override
-        	protected void populateEmptyItem(Item item) {
+			private static final long serialVersionUID = -5912364629079858110L;
+
+			@Override
+        	protected void populateEmptyItem(Item<Square> item) {
         		
         		item.add(new Label("cell", "empty"));
         		
         	}
 
         	@Override
-        	protected void populateItem(Item item) {
+        	protected void populateItem(Item<Square> item) {
         		item.add(new Label("cell", "empty"));
         		
         	}
