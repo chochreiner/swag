e1d88139e1fc146440189fc0342fda0fa2c1944f
Fader A. Vader <superascio@mailinator.com>
2011-06-03 20:44:13 +0200

    Added code generation to swag-model

---
+++ swag-model/src/main/java/at/ac/tuwien/swag/domain/AbstractEntity.java
@@ -0,0 +1,54 @@
+package at.ac.tuwien.swag.domain;
+
+import java.util.Iterator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+	@Id
+	@GeneratedValue
+	public long getId()          { return id;    }
+	public void setId( long id ) { this.id = id; }
+	
+	public String toString() {
+		return "<" + getClass().getSimpleName() + ":" + id + ">";
+	}
+	
+	//*** PRIVATE PARTS 
+	
+	private long id;
+
+	//*** TO STRING HELPERS FOR SUBCLASSES
+	
+	protected static String toString( Object o ) {
+		if ( o == null )
+			return null;
+		if ( o instanceof AbstractEntity ) {
+			AbstractEntity e = (AbstractEntity) o;
+			return e.getClass().getSimpleName() + "[id: " + e.id + "]";
+		}
+		return o.toString();
+	}
+	protected static <E extends AbstractEntity> String toString( Iterable<E> es ) {
+		if ( es == null ) return null;
+		
+		String      out = "[";
+		Iterator<E> it  = es.iterator();
+		
+		if ( !it.hasNext() ) return "[]";
+		
+		out += it.next();
+		
+		while ( it.hasNext() ) {
+			out += ", " + it.next();
+		}
+		
+		return out + "]";
+	}
+	
+}
