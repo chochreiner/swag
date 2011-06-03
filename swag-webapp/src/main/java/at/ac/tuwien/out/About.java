f76b9230f9d5ee43f033135c82b1b4a957352025
Christoph Hochreiner <ch.hochreiner@gmail.com>
2011-06-03 21:13:11 +0200

    some adeptions

---
+++ swag-webapp/src/main/java/at/ac/tuwien/out/About.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.out;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class About extends OutPage {
+    private static final long serialVersionUID = -6712153643198619949L;
+
+    public About(PageParameters parameters) {
+        super(parameters);
+    }
+}
