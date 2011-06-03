6db3e0ddec7c1c3fbe12c0635e823a95483a8284
Christoph Hochreiner <ch.hochreiner@gmail.com>
2011-06-03 21:01:57 +0200

    base register
    Signed-off-by: Christoph Hochreiner <ch.hochreiner@gmail.com>

--- swag-webapp/src/main/java/at/ac/tuwien/out/Register.java
+++ swag-webapp/src/main/java/at/ac/tuwien/out/Register.java
@@ -4,17 +4,17 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import at.ac.tuwien.out.form.LoginForm;
+import at.ac.tuwien.out.form.RegisterForm;
 
-public class Login extends OutPage {
+public class Register extends OutPage {
     private static final long serialVersionUID = -6712153643198619949L;
 
-    public Login(PageParameters parameters) {
+    public Register(PageParameters parameters) {
         super(parameters);
         FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
         add(feedbackPanel);
 
-        Form<?> loginForm = new LoginForm("loginForm");
-        add(loginForm);
+        Form<?> registerForm = new RegisterForm("registerForm");
+        add(registerForm);
     }
 }
