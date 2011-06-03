0f6c9e6011a0bd7100a0dbc28ed68aeaabd5122c
Christoph Hochreiner <ch.hochreiner@gmail.com>
2011-06-03 20:37:13 +0200

    extracted form

--- swag-webapp/src/main/java/at/ac/tuwien/out/Login.java
+++ swag-webapp/src/main/java/at/ac/tuwien/out/Login.java
@@ -1,29 +1,20 @@
 package at.ac.tuwien.out;
 
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import at.ac.tuwien.out.form.LoginForm;
+
 public class Login extends OutPage {
+    private static final long serialVersionUID = -6712153643198619949L;
 
     public Login(PageParameters parameters) {
         super(parameters);
         FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
         add(feedbackPanel);
 
-        Form loginForm = new Form("loginForm") {
-            @Override
-            protected void onSubmit() {
-                info("the form was submitted!");
-            }
-        };
-
-        loginForm.add(new TextField<String>("name"));
-        loginForm.add(new PasswordTextField("password"));
-
+        Form<?> loginForm = new LoginForm("login");
         add(loginForm);
-
     }
 }
--- swag-webapp/src/main/java/at/ac/tuwien/out/OutNavigation.java
+++ swag-webapp/src/main/java/at/ac/tuwien/out/OutNavigation.java
@@ -4,10 +4,11 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class OutNavigation extends Panel {
+    private static final long serialVersionUID = 3384116956231703356L;
 
     public OutNavigation(String id) {
         super(id);
-        add(new BookmarkablePageLink("login", Login.class));
+        add(new BookmarkablePageLink<String>("login", Login.class));
     }
 
 }
