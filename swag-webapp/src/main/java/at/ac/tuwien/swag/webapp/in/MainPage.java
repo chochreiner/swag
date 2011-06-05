dec96eadb145099775c170a771da56e1a9b4787f
Fader A. Vader <superascio@mailinator.com>
2011-06-05 20:17:01 +0200

    Changed config of database to get PostgreSQL running.

--- swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/MainPage.java
+++ swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/MainPage.java
@@ -1,12 +1,25 @@
 package at.ac.tuwien.swag.webapp.in;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+@AuthorizeInstantiation({"ADMIN", "USER"})
 public class MainPage extends InPage {
     private static final long serialVersionUID = -6454931055063290217L;
 
     public MainPage(PageParameters parameters) {
         super(parameters);
+        
+        this.add( 
+        	new Label( "message", em.createQuery( "SELECT u FROM User u" ).getResultList().toString() )
+        );
     }
 
+    @PersistenceContext
+    private EntityManager em;
+    
 }
