32b982c78625808426d394c4327de62ba262041d
Felix Rinker <Felix.Rinker@student.tuwien.ac.at>
2011-06-09 02:48:28 +0200

    Implemented create new map form.

--- swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/form/CreateMapForm.java
+++ swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/form/CreateMapForm.java
@@ -1,13 +1,33 @@
 package at.ac.tuwien.swag.webapp.in.form;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.validation.validator.RangeValidator;
 
-public class CreateMapForm extends Form<Void> {
-    private static final long serialVersionUID = 6040480253914226510L;
+import com.google.inject.Inject;
 
+import at.ac.tuwien.swag.model.dao.MapDAO;
+import at.ac.tuwien.swag.model.dao.SquareDAO;
+import at.ac.tuwien.swag.model.domain.Map;
+import at.ac.tuwien.swag.model.domain.Square;
+
+public class CreateMapForm extends Form<Void> {
+    
+	private static final long serialVersionUID = 6040480253914226510L;
+
+	@Inject
+	private MapDAO mapDao;
+	
+	@Inject
+	private SquareDAO squareDao;
+	
+	
     private RequiredTextField<String> name;
     private RequiredTextField<Integer> width;
     private RequiredTextField<Integer> height;
@@ -38,6 +58,44 @@ public class CreateMapForm extends Form<Void> {
 
     @Override
     protected void onSubmit() {
-        info("TBD");
+    	try{
+    		mapDao.findByName(name.getValue());
+    		info("The Map name "+name.getValue()+"is already in use");
+    		
+    	}catch(NoResultException e) {
+    		int startX = 1;
+        	int endX = Integer.valueOf(width.getValue());
+        	int startY = 1;
+        	int endY = Integer.valueOf(height.getValue());
+        	
+            Map map = new Map();
+           
+            map.setName(name.getValue());
+            map.setXSize(endX);
+            map.setYSize(endY);
+            map.setMaxNumUsers(Integer.valueOf(user.getValue()));
+
+            List<Square> squares = new ArrayList<Square>();
+           
+            for (int y = startY; y <= endY; y++) {
+            	for (int x = startX; x <= endX; x++) {
+    	            Square square = new Square();
+    	            square.setCoordX(x);
+    	            square.setCoordY(y);
+    	            square.setMap(map);
+    	            square.setIsHomeBase(false);
+    	            squares.add(square);
+            	}
+            }
+        	
+        	 mapDao.beginTransaction();
+         		mapDao.insert(map);
+         		for (Square sq : squares) {
+         			squareDao.insert(sq);
+         		}
+         	mapDao.commitTransaction();
+        	 	
+            info("The new Map "+name.getValue()+" was created");
+    	}
     }
 }
