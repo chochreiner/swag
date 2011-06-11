68c10203c43dfec2a30a772d1e8e99032b35b28b
Felix Rinker <Felix.Rinker@tuwien.ac.at>
2011-06-11 16:20:24 +0200

    Switched admin navigation from side to top

--- swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/InPage.java
+++ swag-webapp/src/main/java/at/ac/tuwien/swag/webapp/in/InPage.java
@@ -5,13 +5,22 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import at.ac.tuwien.swag.webapp.SwagWebSession;
+
 @AuthorizeInstantiation({ Roles.ADMIN, Roles.USER })
 public abstract class InPage extends WebPage {
 	private static final long serialVersionUID = 7723393016017265570L;
 	
 	public InPage(PageParameters parameters) {
+		SwagWebSession session = (SwagWebSession) getSession();
 		add(new InTopNavigation("topNavigation"));
 		add(new InNavigation("mainNavigation"));
-		add(new InSideNavigation("sideNavigation"));
+		
+		AdminNavigation adminNav = new AdminNavigation("adminNavigation");
+		adminNav.setVisible(false);
+		if(session.getRoles().hasRole(Roles.ADMIN)) {
+			adminNav.setVisible(true);
+		}
+		add(adminNav);
 	}
 }
