224309a1cf54a18823aa2a0ecf696d40aa740e92
Fader A. Vader <superascio@mailinator.com>
2011-06-12 19:58:37 +0200

    Failed at implementing a working mail notification system.

--- swag-notification/src/main/java/at/ac/tuwien/swag/mail/MailNotificationBean.java
+++ swag-notification/src/main/java/at/ac/tuwien/swag/mail/MailNotificationBean.java
@@ -1,15 +1,14 @@
 package at.ac.tuwien.swag.mail;
 
-import at.ac.tuwien.swag.model.dto.MessageDTO;
-import at.ac.tuwien.swag.model.dto.UserDTO;
+import at.ac.tuwien.swag.messages.email.EmailRequest;
 import at.ac.tuwien.swag.util.MessageHandler;
 
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.jms.JMSException;
-import javax.mail.MessagingException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @MessageDriven(mappedName = "swag.queue.Notification", activationConfig = {
     @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
@@ -17,20 +16,18 @@ import org.slf4j.LoggerFactory;
 })
 public class MailNotificationBean extends MessageHandler {
     
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+//    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @EJB
     private MailService mailService;
     
     public MailNotificationBean() {
     }
     
-    public void handle( MessageDTO msg ) throws JMSException {
-        for (UserDTO toUser : msg.getTo()) {
-            try {
-                mailService.sendMessage( msg.getFrom().getEmail(), toUser.getEmail(), msg.getSubject(), msg.getText());
-            } catch (MessagingException ex) {
-                logger.error(ex.getMessage());
-            }
-        }
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public void handle( EmailRequest msg ) throws JMSException {
+    	mailService.sendMessage( msg.from, msg.emailAddress, msg.subject, msg.text );
+    	
+    	reply( false );
     }
-
 }
